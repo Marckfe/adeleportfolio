cee4e854139e4c35e3c5183f17594787c2704d02
Marckfe <sevenmarco@hotmail.it>
2026-05-28 18:38:12 +0200

    Merge missing pages into The Adventures of Nori project and remove redundant placeholders

--- js/admin.js
+++ js/admin.js
@@ -23,12 +23,12 @@ async function checkLogin() {
 }
 
 async function loadAdminData() {
-    const localData = localStorage.getItem('adele_portfolio_data_v16');
+    const localData = localStorage.getItem('adele_portfolio_data_v17');
     if (localData) {
         portfolioData = JSON.parse(localData);
         renderAdminList();
     } else {
-        fetch('projects.json?v=16')
+        fetch('projects.json?v=17')
             .then(res => res.json())
             .then(data => {
                 portfolioData = data;
@@ -163,7 +163,7 @@ function deleteProject(id) {
 }
 
 function saveDataLocally() {
-    localStorage.setItem('adele_portfolio_data_v16', JSON.stringify(portfolioData));
+    localStorage.setItem('adele_portfolio_data_v17', JSON.stringify(portfolioData));
 }
 
 // Export JSON
